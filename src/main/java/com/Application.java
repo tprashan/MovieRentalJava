9aa651612f4b9573ecee860729427d0390814662
tprashan <tprashan@thoughtworks.com>
2018-04-24 18:44:34 +0530

    write unit test for movie rantal controller

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -6,9 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = {
-        "com.prashant.repositories"
-})
 @EntityScan(basePackages = {
         "com.prashant.model"
 })
--- src/test/java/com/prashant/controllers/MovieRentalControllerSpec.java
+++ src/test/java/com/prashant/controllers/MovieRentalControllerSpec.java
@@ -1,37 +1,55 @@
 package com.prashant.controllers;
 
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.prashant.model.Movie;
+import com.prashant.services.MovieRentalService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = MovieRentalController.class)
 public class MovieRentalControllerSpec {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private MovieRentalService movieRentalService;
+
     @Test
     public void whenNumber_thenAutoType() throws Exception {
-        Map<String,String> params = new HashMap<>();
-        params.put("genre","Adventure");
-        params.put("year","1995");
+        Movie movie1 = new Movie("4261,Adventure,Iron Will (1994)");
+        Movie movie2 = new Movie("5239,Adventure,Prince Valiant (1954)");
+        List<Movie> movieList = new ArrayList<>();
+        movieList.add(movie1);
+        movieList.add(movie2);
 
-//        Movie movie1 = new Movie("1,Toy Story (1995),Adventure|Animation|Children|Comedy|Fantasy");
-//        Movie movie2 = new Movie("2,Jumanji (1995),Adventure|Children|Fantasy");
-//        List<Movie> movieList = new ArrayList<>();
-//        movieList.add(movie1);
-//        movieList.add(movie2);
-//
-//        when(videoRental.getMovies()).thenReturn(movieList);
-//
-//        this.mockMvc.perform(get("/allMovies")
-//                .param("genre", "Adventure")
-//                .param("title", "Toy Story")
-//                .param("year", "1995"))
-//                .andExpect(status().isOk());
+        when(movieRentalService.getAllMoviesFromFile()).thenReturn(movieList);
 
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
+                "/allMoviesFromFile").accept(
+                MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+        String expected = "[[ \"4261\", \"Adventure\", \"Iron Will (1994)\" ], [ \"5239\", \"Adventure\", \"Prince Valiant (1954)\" ]]";
+
+        JSONAssert.assertEquals("[]", Arrays.toString(result.getResponse().getContentAsByteArray()), false);
     }
 }
